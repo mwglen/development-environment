81b85ec97b0bd89c76953c687492f0a612cb261a
mwg2202 <mwg2202@yahoo.com>
2021-03-24 18:49:38 -0400

    Fixed init.sh and added README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+#nix-configuration
+## My configuration files for my current nixos environment.
+
+## To use: `\init.sh`
--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
-cp configuration.nix /etc/nixos/configuration.nix
-cp syschdemd.nix /etc/nixos/syschdemd.nix
-cp syschdemd.sh /etc/nixos/syschdemd.sh
+sudo cp configuration.nix /etc/nixos/configuration.nix
+sudo cp syschdemd.nix /etc/nixos/syschdemd.nix
+sudo cp syschdemd.sh /etc/nixos/syschdemd.sh
+sudo nixos-rebuild switch
 
+mkdir -p /home/mwglen/.config/nixpkgs/p10k-config
 cp home.nix ~/.config/nixpkgs/home.nix
-cp p10k.zsh ~/.config/nixpkgs/p10k.zsh
-
+cp p10k.zsh ~/.config/nixpkgs/p10k-config/p10k.zsh
+home-manager switch
