4d785ea9daf05f81a630a01453b566299e22af0c
mwg2202 <mwg2202@yahoo.com>
2021-03-24 18:15:15 -0400

    Moved config files to the root of the repository and added a script to set up nixos

---
+++ init.sh
@@ -0,0 +1,7 @@
+cp configuration.nix /etc/nixos/configuration.nix
+cp syschdemd.nix /etc/nixos/syschdemd.nix
+cp syschdemd.sh /etc/nixos/syschdemd.sh
+
+cp home.nix ~/.config/nixpkgs/home.nix
+cp p10k.zsh ~/.config/nixpkgs/p10k.zsh
+
