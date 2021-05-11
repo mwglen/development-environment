b9eed6d612b8399538800b5b3e3a13d7c87724da
mwg2202 <mwg2202@yahoo.com>
2021-05-11 16:43:47 -0400

    Added code to set zsh as default shell in from-ubuntu.sh

--- from-ubuntu.sh
+++ from-ubuntu.sh
@@ -6,10 +6,15 @@ curl -L https://nixos.org/nix/install | sh
 
 # Enable flakes
 nix-env -iA nixpkgs.nixUnstable
-echo "experimental-features = nix-command flakes" > $HOME/.config/nix/nix.conf
+echo "experimental-features = nix-command flakes" >> $HOME/.config/nix/nix.conf
 
 # Remove apt's version of git
 sudo apt remove git
 
 # Initialize home-manager
 ./update-hm.sh
+
+# Add zsh
+echo "$HOME/.nix-profile/bin/zsh" | sudo tee -a /etc/shells
+echo "chsh -s $HOME/.nix-profile/bin/zsh" >> $HOME/.bashrc
+echo "source $HOME/.profile" | sudo tee -a $HOME/.zshrc
