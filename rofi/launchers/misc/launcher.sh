9fd46e86070808d083a598f418cf8fb5e9917167
Matt Glen <mwg2202@yahoo.com>
2022-01-24 10:03:03 -0500

    Removed some unwanted themes from rofi directory and switched to using xbindkeys for key bindings independent of window manager

--- rofi/launchers/misc/launcher.sh
+++ rofi/launchers/misc/launcher.sh
@@ -8,15 +8,9 @@
 # Available Styles
 # >> Created and tested on : rofi 1.6.0-1
 #
-# blurry	blurry_full		kde_simplemenu		kde_krunner		launchpad
-# gnome_do	slingshot		appdrawer			appdrawer_alt	appfolder
-# column	row				row_center			screen			row_dock		row_dropdown
+# blurry	kde_simplemenu
 
-theme="screen"
+theme="blurry"
 dir="$HOME/.config/rofi/launchers/misc"
-
-# comment these lines to disable random style
-themes=($(ls -p --hide="launcher.sh" $dir))
-theme="${themes[$(( $RANDOM % 16 ))]}"
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
